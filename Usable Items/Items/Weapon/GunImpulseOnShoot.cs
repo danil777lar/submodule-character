43498bdcf59d06d1f2e85cf5ae5d38bd18a12f8d
danya <danil777lar@gmail.com>
2025-08-21 18:38:45 +0400

    add impulse on shoot

---
+++ Usable Items/Items/Weapon/GunImpulseOnShoot.cs
@@ -0,0 +1,24 @@
+using Cinemachine;
+using Larje.Character;
+using UnityEngine;
+
+public class GunImpulseOnShoot : MonoBehaviour
+{
+    [SerializeField] private FPSCameraShakeAnimation shootAnim; 
+    
+    private GunUsableItem _gun;
+    private FPSCameraShaker _shaker;
+    
+    private void Start()
+    {
+        _gun = GetComponent<GunUsableItem>();
+        _shaker = GetComponentInParent<Character>().GetComponentInChildren<FPSCameraShaker>();
+
+        _gun.EventShoot += OnShoot;
+    }
+
+    private void OnShoot()
+    {
+        _shaker.AddShake(new FPSCameraShaker.Shake(shootAnim, false, () => 1f, () => 1f));
+    }
+}
