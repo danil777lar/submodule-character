15d476639fb2a07e6e4d730d290d7dd20484a6d5
danya <danil777lar@gmail.com>
2025-08-21 17:35:09 +0400

    add pistol paralax, add autop pistol set to player

---
+++ Usable Items/Items/Weapon/GunLookAtCursor.cs
@@ -0,0 +1,26 @@
+using System;
+using Larje.Core;
+using UnityEngine;
+
+public class GunLookAtCursor : MonoBehaviour
+{
+    [SerializeField] private float speed = 10f;
+    
+    [InjectService] private CursorService _cursorService;
+    
+    private void Start()
+    {
+        DIContainer.InjectTo(this);
+        _cursorService.AddCursorUpdatedListener(OnCursorUpdated, CursorService.CallbackUpdateType.AfterUpdate);
+    }
+
+    private void OnDestroy()
+    {
+        _cursorService.RemoveCursorUpdatedListener(OnCursorUpdated);
+    }
+
+    private void OnCursorUpdated(float deltaTime)
+    {
+        transform.forward = Vector3.Lerp(transform.forward, _cursorService.Direction, deltaTime * speed);
+    }
+}
