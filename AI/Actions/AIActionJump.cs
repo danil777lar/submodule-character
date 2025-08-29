29a6845529e0c292f0162288f8e1576f89ec6f4d
danya <danil777lar@gmail.com>
2025-08-29 17:34:37 +0400

    add aiactionjump

---
+++ AI/Actions/AIActionJump.cs
@@ -0,0 +1,63 @@
+using Larje.Character.Abilities;
+using Larje.Character.AI;
+using UnityEngine;
+
+public class AIActionJump : AIAction
+{
+    [SerializeField] private int inputPriority = 100;
+
+    private bool _pressed = false;
+    private bool _released = false;
+    private CharacterJump _characterJump;
+    private CharacterJump.JumpInput _input; 
+    
+    public override void PerformAction()
+    {
+        
+    }
+
+    protected override void OnInitialized()
+    {
+        _characterJump = Brain.Owner.GetComponentInChildren<CharacterJump>();
+    }
+
+    protected override void OnEnterState()
+    {
+        _characterJump.Input.AddValue(GetInput, () => inputPriority);
+        _pressed = true;
+    }
+
+    protected override void OnExitState()
+    {
+        _characterJump.Input.RemoveValue(GetInput);
+    }
+
+    private void Update()
+    {
+        UpdateInput();
+    }
+
+    private void UpdateInput()
+    {
+        /*if (_pressed)
+        {
+            _pressed = false;
+            _released = true;
+            return;
+        }
+
+        if (_released)
+        {
+            _released = false;
+        }*/
+    }
+
+    private CharacterJump.JumpInput GetInput()
+    {
+        if (_input == null)
+        {
+            _input = new CharacterJump.JumpInput(() => _pressed, () => _released);
+        }
+        return _input;
+    }
+}
