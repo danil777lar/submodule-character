c998efafe541ad1519a101f4a872a539ad7bc71a
danya <danil777lar@gmail.com>
2025-09-28 17:32:15 -0300

    update rojectile

--- Usable Items/Items/Weapon/Projectile/BulletProjectile.cs
+++ Usable Items/Items/Weapon/Projectile/BulletProjectile.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
 
@@ -7,19 +8,27 @@ public class BulletProjectile : Projectile
     [SerializeField] private int damage = 10;
     [Space]
     [SerializeField] private float speed = 100f;
+    [SerializeField] private float speedDrag = 0f;
+    [SerializeField] private float gravity = 0f;
     [SerializeField] private float lifeTime = 2f;
     [SerializeField] private float destroyDelay = 0.1f;
     [SerializeField] private GameObject modelHolder;
     [SerializeField] private LayerMask hitLayer;
     [Space] 
+    [SerializeField] private List<GameObject> spawnOnHit;
+    [Space] 
     [SerializeField] private UnityEvent onHit;
 
+    private float _currentSpeed;
+    private float _currentGravity;
     private bool _destroying;
     private float _currentLifetime;
     
     public void Init(Vector3 position, Vector3 direction, Vector3 modelStartPosition)
     {
         _currentLifetime = lifeTime;
+        _currentSpeed = speed;
+        _currentGravity = 0f;
         
         transform.position = position;
         transform.forward = direction;
@@ -55,12 +64,17 @@ public class BulletProjectile : Projectile
 
     private void Move()
     {
+        _currentSpeed = _currentSpeed * (1f - speedDrag * Time.fixedDeltaTime);
+        _currentGravity -= gravity * Time.fixedDeltaTime;
+        
+        Vector3 velocity = transform.forward * _currentSpeed + Vector3.up * _currentGravity;
+        
         bool hitten = false;
-        Vector3 position = transform.position + transform.forward * (speed * Time.fixedDeltaTime);
-        Vector3 direction = transform.forward;
+        Vector3 position = transform.position + velocity * Time.fixedDeltaTime;
+        Vector3 direction = velocity.normalized;
         
         Ray ray = new Ray(transform.position, transform.forward);
-        if (Physics.Raycast(ray, out RaycastHit hit, speed * Time.fixedDeltaTime, hitLayer))
+        if (Physics.Raycast(ray, out RaycastHit hit, velocity.magnitude * Time.fixedDeltaTime, hitLayer))
         {
             position = hit.point;
             direction = hit.normal; 
@@ -75,24 +89,35 @@ public class BulletProjectile : Projectile
                 hitDirection = transform.forward
             };
             target?.SendDamage(damageData);
+            
+            OnHit(hit.normal);
         }
         
         transform.position = position;
         transform.forward = direction;
         _currentLifetime -= Time.fixedDeltaTime;
-
-        if (hitten)
-        {
-            OnHit();
-        }
-        else if (_currentLifetime <= 0)
+        
+        if (!hitten && _currentLifetime <= 0)
         {
             OnEndLifetime();
         }
     }
 
-    private void OnHit()
+    private void OnHit(Vector3 normal)
     {
+        foreach (GameObject prefab in spawnOnHit)
+        {
+            GameObject instance = Instantiate(prefab, transform.parent);
+            instance.SetActive(true);
+            instance.transform.position = transform.position;
+            instance.transform.forward = transform.forward;
+
+            if (instance.TryGetComponent(out Rigidbody rb))
+            {
+                rb.AddForce(normal * ((_currentSpeed + _currentGravity) * 0.25f), ForceMode.VelocityChange);
+            }
+        }
+        
         onHit.Invoke();
         DestroySelf();
     }
